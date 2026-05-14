ab10fbb522e9f7e405b212ac53cea1083c306787
Tirtha Debnath <tirthadebnath722@gmail.com>
2026-05-14 19:42:30 +0600

    install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 echo "[*] Installing dependencies..."
-apt update && apt install python3 python3-pip curl -y
+sudo apt update && sudo apt install python3 python3-pip curl -y
 pip3 install aiohttp beautifulsoup4 lxml --break-system-packages 2>/dev/null || pip3 install aiohttp beautifulsoup4 lxml
 
 echo "[*] Downloading OSINT Email Hunter..."
-curl -sSL -o /usr/local/bin/hunter githubusercontent.com
+# RAW গিটহাব ইউআরএল থেকে সরাসরি 'hunter' কোডটি ডাউনলোড করে সিস্টেমে সেটআপ করবে
+sudo curl -sSL -o /usr/local/bin/hunter githubusercontent.com
 
 echo "[*] Setting executable permissions..."
-chmod +x /usr/local/bin/hunter
+sudo chmod +x /usr/local/bin/hunter
 
 echo "[✔] Setup Complete! You can now use the 'hunter' command anywhere."
